31ef84fbc820ac338db1fb5894d8d9607155ae83
Adron Hall <adron@thrashingcode.com>
2025-04-15 08:37:52 -0700

    Initial Commit

---
+++ force-observations/force_observationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  force_observationsApp.swift
+//  force-observations
+//
+//  Created by Adron Hall on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct force_observationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ force-observationsTests/force_observationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  force_observationsTests.swift
+//  force-observationsTests
+//
+//  Created by Adron Hall on 4/15/25.
+//
+
+import Testing
+@testable import force_observations
+
+struct force_observationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
